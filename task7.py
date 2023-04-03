a644e88f25092e73c951880173a97f8251b88495
dRamee <maksim.yolckin@yandex.com>
2023-04-04 01:57:15 +0700

    some fixes in tasks 7, 8

--- task7.py
+++ task7.py
@@ -1,135 +1,68 @@
 from random import randint
 
 
-def merge(array, start1, end1, start2, end2, buff_ind):
+def merge(array, l1, r1, l2, r2, buff_ind):
 
-    # merge two parts 'till don't go to the of the edges
-    while start1 < end1 and start2 < end2:
-
-        if array[start1] < array[start2]:
-            array[buff_ind], array[start1] = array[start1], array[buff_ind]
-            start1 += 1
-            buff_ind += 1
+    while l1 < r1 and l2 < r2:
+        if array[l1] < array[l2]:
+            array[l1], array[buff_ind] = array[buff_ind], array[l1]
+            l1 += 1
         else:
-            array[buff_ind], array[start2] = array[start2], array[buff_ind]
-            start2 += 1
-            buff_ind += 1
-
-    # complete merge if not all elements went into buffer
-    while start1 < end1:
-        array[start1], array[buff_ind] = array[buff_ind], array[start1]
-        start1 += 1
+            array[l2], array[buff_ind] = array[buff_ind], array[l2]
+            l2 += 1
         buff_ind += 1
 
-    while start2 < end2:
-        array[start2], array[buff_ind] = array[buff_ind], array[start2]
-        start2 += 1
+    while l1 < r1:
+        array[l1], array[buff_ind] = array[buff_ind], array[l1]
+        l1 += 1
+        buff_ind += 1
+
+    while l2 < r2:
+        array[l2], array[buff_ind] = array[buff_ind], array[l2]
+        l2 += 1
         buff_ind += 1
 
 
-def merge_sort_impl(array, r, l, buff_ind=None):
+def merge_sort_impl(array, l, r, buff_ind):
+    if r - l > 1:
 
-    mid = (l + r) // 2
+        mid = l + (r - l) // 2
 
-    merge_sort_impl(array, l, mid, mid + 1)
-    merge_sort_impl(array, l, mid)
-    merge(array, l, mid, mid + 1, r, buff_ind)
+        merge_sort_inplace(array, l, mid)
+        merge_sort_inplace(array, mid, r)
+        merge(array, l, mid, mid, r, buff_ind)
+    else:
+        array[l], array[buff_ind] = array[buff_ind], array[l]
 
 
 def merge_sort_inplace(array, l, r):
 
+    if r - l > 1:
+        m = l + (r - l) // 2
+        buffer = l + r - m
+
+        merge_sort_impl(array, l, m, buffer)
+
+        while buffer - l > 2:
+            n = buffer
+            buffer = l + (n - l + 1) // 2
+            merge_sort_impl(array, buffer, n, l)
+            merge(array, l, l + n - buffer, n, r, buffer)
+
+        n = buffer
+        while n > l:
+            m = n
+            while m < r and array[m] < array[m - 1]:
+                array[m], array[m - 1] = array[m - 1], array[m]
+                m += 1
+            n -= 1
 
 
+for i in range(128, 2 ** 12):
+    test1 = [randint(0, 1000) for _ in range(i)]
+    test2 = test1.copy()
+    test2.sort()
 
+    merge_sort_inplace(test1, 0, i)
 
-
-
-# # l, mid, mid + 1, r,buff,nums
-# def merge(l1, r1, buff_l, buff_r, l2, r2, nums, is_merge_buff):
-#     # лев сортнут, буффер сортнут,правый нет
-#     # сливаем левый с буфером, используя как буфер средний и буфер
-#     # [l2,r2] может быть на 1 длиньше [l1,r1]
-#     i, j, k = l1, buff_l, l2
-#     while i <= r1 and j <= buff_r:
-#         if nums[i] < nums[j]:
-#             nums[i], nums[k] = nums[k], nums[i]
-#             i += 1
-#         else:
-#             nums[j], nums[k] = nums[k], nums[j]
-#             j += 1
-#         k += 1
-#     while i <= r1:
-#         nums[i], nums[k] = nums[k], nums[i]
-#         i += 1
-#         k += 1
-#     if is_merge_buff:
-#         while j <= buff_r:
-#             nums[j], nums[k] = nums[k], nums[j]
-#             j += 1
-#             k += 1
-#
-#
-# def merge_sort(l1, r1, l2, r2, nums):
-#     # сорчу [l1,r1] используя [l2,r2] буфером.
-#     # [l2,r2](r2-1) должен стат сортнутым и поменяться местами с [l1,r1]
-#     # буфер может быть на 1 длиньше массива
-#     if r1 - l1 + 1 < 1:
-#         return
-#     if r1 - l1 + 1 == 1:
-#         nums[l1], nums[l2] = nums[l2], nums[l1]
-#         return
-#     if r1 - l1 + 1 == 2:
-#         if nums[l1] > nums[r1]:
-#             nums[l1], nums[r1] = nums[r1], nums[l1]
-#         return
-#     mid = (l1 + r1) // 2
-#     merge_sort(l1, mid, l2, r2, nums)
-#     merge_sort(mid + 1, r1, l2, r2, nums)
-#     merge(l1, mid, mid + 1, r1, l2, r2, nums, True)  # [l1,mid],[mid+1,r1] сортнуты
-#     i, j = l1, l2
-#     while i <= r1:
-#         nums[i], nums[j] = nums[j], nums[i]
-#         i += 1
-#         j += 1
-#     # поменялись местами возвращаю в исходные
-#     # в итоге получаю сортнутый [l1,mid]
-#
-#
-# def sort(l, r, buff_l, buff_r, nums):
-#     if l > r:
-#         return
-#     if l == r:  # буффер(сортированный) это массив без левого эл-та, nums[0] -тот самый
-#         tmp = nums[0]
-#         i = 1
-#         while i < len(nums) and tmp > nums[i]:
-#             nums[i - 1] = nums[i]
-#             i += 1
-#         nums[i - 1] = tmp
-#         return
-#     mid = (l + r) // 2  # длины лев и сред равны или длина лев на 1 больше
-#     fl = (mid - l + 1) > (r - (mid + 1) + 1)
-#     merge_sort(mid + 1, r, l, mid, nums)  # сорчу [mid+1,r] используя [l,mid] буфером.
-#     i, j = l, mid + 1
-#     while j <= r:
-#         nums[i], nums[j] = nums[j], nums[i]
-#         i += 1
-#         j += 1
-#     # [l,mid](mid-1) должен стат сортнутым
-#     if fl:  # если лев был длинее прав, то по сути я меняю длины местами
-#         mid -= 1
-#     fl2 = (r - (mid + 1) + 1) > (mid - l + 1)
-#     merge(l, mid, buff_l, buff_r, mid + 1 + fl2, r, nums, False)  # лев сортнут, серединный нет,буффер сортнут
-#     # сливаем левый с буфером, используя как буфер средний и буфер
-#     sort(l, mid + fl, mid + 1 + fl, buff_r, nums)  # идём рекурсивно 2 два раза уменьшая длину...
-#
-#
-# def sort_array(nums):
-#     l, r, buff = 0, len(nums) - 2, len(nums) - 1  # буффер из одного эл-та
-#     sort(l, r, buff, buff, nums)
-
-
-test = [randint(0, 200) for _ in range(8)]
-
-merge_sort_inplace(test, 0, len(test))
-
-print(test)
+    assert test1 == test2
--- task8.py
+++ task8.py
@@ -1,43 +1,52 @@
-test_arr2 = [4, 3, 2, 1]
+from random import randint
+
+test = [3, 2, 1]
 
 
-def merge_and_cnt(arr, start1, end1, end2):
-    start2 = end1 + 1
+def merge_and_cnt(array, left, mid, right):
+
+    new_arr = [0] * (right - left)
+    i = left
+    j = mid
     k = 0
-    if arr[end1] <= arr[start2]:
-        return 0
+    inversions = 0
 
-    while start1 <= end1 and start2 <= end2:
-
-        if arr[start1] <= arr[start2]:
-            start1 += 1
+    while i < mid and j < right:
+        if array[i] < array[j]:
+            new_arr[k] = array[i]
+            i += 1
         else:
-            value = arr[start2]
-            index = start2
+            new_arr[k] = array[j]
+            j += 1
+            inversions += mid - i
+        k += 1
 
-            while index != start1:
-                arr[index] = arr[index - 1]
-                index -= 1
+    while i < mid:
+        new_arr[k] = array[i]
+        i += 1
+        k += 1
 
-            arr[start1] = value
+    while j < right:
+        new_arr[k] = array[j]
+        j += 1
+        k += 1
 
-            start1 += 1
-            end1 += 1
-            start2 += 1
-            k += (end1 - start1 + 1)
-    return k
+    for i in range(left, right):
+        array[i] = new_arr[i - left]
+
+    return inversions
 
 
-def merge_sort_and_cnt(arr, l, r):
-    t1 = t2 = t3 = 0
-    if l < r:
-        m = l + (r - l) // 2
+def merge_sort_and_cnt(array, l, r):
+    inversions = 0
+    if r - l > 1:
 
-        t1 = merge_sort_and_cnt(arr, l, m)
-        t2 = merge_sort_and_cnt(arr, m + 1, r)
+        mid = l + (r - l) // 2
 
-        t3 = merge_and_cnt(arr, l, m, r)
-    return t1 + t2 + t3
+        inversions += merge_sort_and_cnt(array, l, mid)
+        inversions += merge_sort_and_cnt(array, mid, r)
+        inversions += merge_and_cnt(array, l, mid, r)
+    return inversions
 
 
 def sol_for_leet(arr):
@@ -47,3 +56,20 @@ def sol_for_leet(arr):
             return False
 
     return True
+
+
+test1 = [5, 4, 3, 2, 1]
+test2 = [1, 2, 3, 4, 5]
+
+print(merge_sort_and_cnt(test1, 0, len(test1)))
+print(merge_sort_and_cnt(test2, 0, len(test2)))
+
+for i in range(16, 32):
+
+    test = [randint(0, 50) for _ in range(i)]
+
+    print(test)
+    print(merge_sort_and_cnt(test, 0, i))
+    input()
+
+
