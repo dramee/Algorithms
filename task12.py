29c3c435b4e739711ab882c0c09743176462e71e
dRamee <maksim.yolckin@yandex.com>
2023-04-04 02:50:34 +0700

    Modified Lomuto partition in 12 task

--- task12.py
+++ task12.py
@@ -1,35 +1,52 @@
 from random import randint
 
 
-def partition(array, l, r):
-    if l == r:
-        return l
+def partition(array, left, right):
+    if left == right:
+        return left, left
 
-    pivot_ind = randint(l, r)
-    array[pivot_ind], array[r] = array[r], array[pivot_ind]
+    pivot_ind = randint(left, right)
+    array[pivot_ind], array[right] = array[right], array[pivot_ind]
 
-    p, pivot = l, array[r]
-    for i in range(l, r):
-        if array[i] < pivot:
-            if i != p:
-                array[i], array[p] = array[p], array[i]
-            p += 1
-    array[p], array[r] = pivot, array[p]
-    return p
+    pivot = array[right]
+    l, h, c = left, left, left
+
+    while c < right:
+
+        if array[c] < pivot:
+            tmp = array[c]
+            array[c] = array[h]
+            array[h] = array[l]
+            array[l] = tmp
+            l += 1
+            h += 1
+
+        elif array[c] == pivot:
+            array[c], array[h] = array[h], array[c]
+            h += 1
+
+        c += 1
+
+    array[right], array[h] = array[h], array[right]
+
+    return l, h
 
 
-def quick_sort(array, l, r):
-    if r < l:
+def quick_sort(array, left, right):
+    if right < left:
         return
-    p = partition(array, l, r)
-    quick_sort(array, l, p - 1)
-    quick_sort(array, p + 1, r)
+
+    l, h = partition(array, left, right)
+
+    quick_sort(array, left, l - 1)
+    quick_sort(array, h + 1, right)
 
 
-test = [randint(0, 100) for _ in range(1024)]
-test_copy = test.copy()
-print(test)
+for i in range(512, 1024):
+    test = [randint(0, 2000) for _ in range(i)]
+    test_copy = test.copy()
 
-quick_sort(test, 0, len(test) - 1)
+    quick_sort(test, 0, len(test) - 1)
+    test_copy.sort()
 
-print(test == sorted(test_copy))
+    assert test_copy == test
